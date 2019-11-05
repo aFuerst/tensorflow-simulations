Side effects suck. Fix bug with improper computation

testing.py
@@ -1,6 +1,16 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
 import tensorflow as tf
+# import shutil
 import sys
+# shutil.rmtree("checkpoints")
+# os.mkdir("checkpoints")
+# force CPU for testing
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 m = 1
 k = 1
 a = -1
@@ -10,11 +20,8 @@ log_freq = 100
 num_iterations = steps // log_freq
 delta_t = totaltime / steps
 prints = []
-log = "file://home/alfuerst/simulations/log.txt"
-tf_log = tf.constant(log)
-log_dir = "/home/alfuerst/simulations/"
-#from tensorflow.python.framework.ops import disable_eager_execution
-#disable_eager_execution()
+log_dir = "/home/alfuerst/simulations/checkpoints"
+log = "/home/alfuerst/simulations/log.out"
 
 def my_print(*args):
     op = tf.print(args, output_stream=log)
@@ -42,11 +49,11 @@ def calc_total_energy(vel, pos):
 
 # @tf.function
 def update_force(pos, vel, force, delta_t):
-    return tf.math.scalar_mul(-k, force)
+    return tf.math.scalar_mul(-k, pos)
 
 #@tf.function
 def update_vel(pos, vel, force, delta_t):
-    t = tf.math.scalar_mul(0.5 * delta_t * m, force)
+    t = tf.math.scalar_mul(0.5 * delta_t / m, force)
     return t + vel
 
 # @tf.function
@@ -59,25 +66,22 @@ def compute_energies(pos, vel, force, energies, delta_t):
     ke = calc_ke(vel, pos)
     pe = calc_pe(vel, pos)
     tot = ke + pe
-    #my_print("kinetic energy returned:", k)
     return (ke, pe, tot)
 
 # @tf.function
-def loop_control(step, vecs):
-    num_steps = log_freq
-    return step < num_steps
+def loop_control(step_target, step, vecs):
+    return step < step_target
 
-# @tf.function
-def loop_execute(step, vecs):
+@tf.function
+def loop_execute(vecs):
     pos, vel, force, energies = vecs
-    step = step + 1
-    vel = update_vel(pos, vel, force, delta_t)
-    pos = update_pos(pos, vel, force, delta_t)
-    force = update_force(pos, vel, force, delta_t)
-    vel = update_vel(pos, vel, force, delta_t)
-    print(vel)
-    energies = compute_energies(pos, vel, force, energies, delta_t)
-    return step, (pos, vel, force, energies)
+    for i in range(log_freq):
+        new_vel = update_vel(pos, vel, force, delta_t)
+        new_pos = update_pos(pos, new_vel, force, delta_t)
+        new_force = update_force(new_pos, new_vel, force, delta_t)
+        new_new_vel = update_vel(new_pos, new_vel, new_force, delta_t)
+        energies = compute_energies(new_pos, new_new_vel, new_force, energies, delta_t)
+    return (new_pos, new_new_vel, new_force, energies)
 
 if __name__ == '__main__':
     pos = tf.Variable(np.array([a, 0, 0], dtype=np.float64), name="position")
@@ -87,21 +91,18 @@ if __name__ == '__main__':
     pe = tf.Variable(0.5*k*a*a, dtype=np.float64, name="potential")
     tot = tf.Variable(0.5*k*a, dtype=np.float64, name="total")
     energies = (ke, pe, tot)
-    step = tf.Variable(0, name="step_counter")
     
     with tf.compat.v1.Session() as sess:
         sess.as_default()
         sess.run(tf.global_variables_initializer())
-        saver = tf.compat.v1.train.Saver([step,pos,vel,force,ke,pe,tot])
-        
+        # saver = tf.compat.v1.train.Saver([pos,vel,force,ke,pe,tot], max_to_keep=None)
+        # checkpoint = tf.train.Checkpoint(pos=pos, vel=vel, force=force, ke=ke, pe=pe, tot=tot)
+        # manager = tf.train.CheckpointManager(checkpoint, directory="/home/alfuerst/simulations/chkpt", max_to_keep=None)
+
         out = (pos, vel, force, energies)
         for seg in range(num_iterations):
-            # for _ in range(log_freq):
-            #     step, out = loop_execute(step, out)
-            step, out = tf.while_loop(loop_control, loop_execute, (step, (pos, vel, force, energies)))
-            step = step * 0
-            print(sess.run(step))
-            print(sess.run(out))
-            #saver.save_counter = seg*log_freq
-            saver.save(sess=sess, save_path='checkpoints/checkpoint', global_step=seg*log_freq, write_meta_graph=False)
-            
+            out = loop_execute(out)
+            out = sess.run(out)
+            print((1+seg)*log_freq, out)
+            # print(saver.save(sess=sess, save_path='checkpoints/{}/'.format((1+seg)*log_freq), global_step=(1+seg)*log_freq))
+            # print(manager.save(checkpoint_number=seg))